a03dfb5690642f6d6cc3267a94ae9f6db954f4a2
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-20 14:58:22 +0200

    understood exercice1 leson11

---
+++ Ejercicio 1/main.js
@@ -0,0 +1,19 @@
+'use strict';
+fetch('https://api.rand.fun/text/string?length=90')
+  .then(function(response) {
+      console.log(response);
+    return response.json();
+
+  })
+  .then(function(data) { 
+    document.body.innerHTML = data.result;
+  });
+//COSAS ENTENDIDAS :: la url base se puede modificar para añadir caracteristicas a lo que deseo buscar.
+
+  ///el console log de response devuelve un OBJETO  cl clas claves ; type: url; redirected; status: ok....etc
+
+  ///el console log de data devuelve cosas raras
+
+  //para pedir informacion distinte de la api debo cambiar la URL
+ // despues de ? las características adicionales de ese tipo de informacion
+
