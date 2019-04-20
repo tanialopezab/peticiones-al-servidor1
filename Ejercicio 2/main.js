aead77242c80f723a252fdc9f85c24661c4015f9
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-20 15:57:28 +0200

    Work in progress: need to check exercice again

--- Ejercicio 2/main.js
+++ Ejercicio 2/main.js
@@ -1,29 +1,42 @@
-// 'use strict';
+'use strict';
 
-// // fetch('https://dog.ceo/api/breeds/image/random')
-// //     .then(function(response){
-// //         console.log(response);
-// //         return response.json();
 
- // })
+// fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(function(response){
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(function(datosJson){
+//         console.log(datosJson);
+//         const img = document.querySelector('img');
+//         img.src = datosJson.message;
+//         Image.alt = 'un perrete';
+//     })
 
- //     .then(function(data){
- //         document.body.innerHTML = data.result;
- //     })
+//línea 16 responde con un objeto con muchos datos
+// linea 20 ya aparece el objeto cons sus claves y sus valores entonces desde ahí ya creo una etiqueta que me almacena el elemento HTML imagen 
+// en img luego coloco en sus atributos el elemento message del json
+// en img coloco en el atributo alt el texto "un perrete";
 
-fetch('https://dog.ceo/api/breeds/image/random')
-    .then(function(response){
-        console.log(response);
-        return response.json();
-    })
-    .then(function(datosJson){
-        console.log(datosJson);
-        const img = document.querySelector('img');
-        img.src = datosJson.message;
-        Image.alt = 'un perrete';
-    })
+///MODIFICACION PARA QUE SOLO SALGAN CHIHUAHUAS
 
-    //línea 16 responde con un objeto con muchos datos
-    // linea 20 ya aparece el objeto cons sus claves y sus valores entonces desde ahí ya creo una etiqueta que me almacena el elemento HTML imagen 
-    // en img luego coloco en sus atributos el elemento message del json
-    // en img coloco en el atributo alt el texto "un perrete";
+const buttonEl = document.querySelector('.btn');
+
+
+function buttonClickRandomImage() {
+
+    fetch('https://dog.ceo/api/breed/Chihuahua/image/random')
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (datosJson) {
+            console.log(datosJson);
+            const img = document.querySelector('.photo');
+            img.src = datosJson.message;
+            Image.alt = 'un chihuahua';
+        })
+}
+buttonEl.addEventListener('click', buttonClickRandomImage);
+
+///LA PETION FALLA, INTENTAR MAS TARDE;
